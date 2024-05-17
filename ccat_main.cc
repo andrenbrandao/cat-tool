130dff15a2c6bdb2fbde74c82b0a59cdc4a84306
André Brandão <andrenbrandao@gmail.com>
2024-05-16 21:49:24 -0300

    refactor: extract PrintFile function

--- ccat_main.cc
+++ ccat_main.cc
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <string>
 
+void PrintFile(std::istream &input_stream) {
+  std::string strRead{};
+  while (std::getline(input_stream, strRead)) {
+    std::cout << strRead << '\n';
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     std::cout << "Must add a filename" << std::endl;
@@ -10,21 +17,14 @@ int main(int argc, char *argv[]) {
   const std::string &filename = argv[1];
 
   if (filename == "-") {
-    std::string strRead{};
-    while (std::getline(std::cin, strRead)) {
-      std::cout << strRead << '\n';
-    }
+    PrintFile(std::cin);
   } else {
     std::ifstream file{filename};
     if (!file) {
       std::cerr << "Could not open file " + filename << std::endl;
       return 1;
     }
-
-    std::string strRead{};
-    while (std::getline(file, strRead)) {
-      std::cout << strRead << '\n';
-    }
+    PrintFile(file);
   }
 
   return 0;
