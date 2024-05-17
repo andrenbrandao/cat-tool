aac7b82dcc8ffb476fd7da37ecbf19db9ff437bf
André Brandão <andrenbrandao@gmail.com>
2024-05-16 21:53:14 -0300

    feat: accept multiple files as input

--- ccat_main.cc
+++ ccat_main.cc
@@ -14,17 +14,20 @@ int main(int argc, char *argv[]) {
     std::cout << "Must add a filename" << std::endl;
     return 0;
   }
-  const std::string &filename = argv[1];
 
-  if (filename == "-") {
-    PrintFile(std::cin);
-  } else {
-    std::ifstream file{filename};
-    if (!file) {
-      std::cerr << "Could not open file " + filename << std::endl;
-      return 1;
+  for (int i = 1; i < argc; i++) {
+    const std::string &filename = argv[i];
+
+    if (filename == "-") {
+      PrintFile(std::cin);
+    } else {
+      std::ifstream file{filename};
+      if (!file) {
+        std::cerr << "Could not open file " + filename << std::endl;
+        return 1;
+      }
+      PrintFile(file);
     }
-    PrintFile(file);
   }
 
   return 0;
