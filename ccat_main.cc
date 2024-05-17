d4de53a7322005523319d9454bd549cf227f89e0
André Brandão <andrenbrandao@gmail.com>
2024-05-16 21:25:44 -0300

    feat: read from stdin

--- ccat_main.cc
+++ ccat_main.cc
@@ -7,17 +7,24 @@ int main(int argc, char *argv[]) {
     std::cout << "Must add a filename" << std::endl;
     return 0;
   }
-  std::string filename = argv[1];
+  const std::string &filename = argv[1];
 
-  std::ifstream file{filename};
-  if (!file) {
-    std::cerr << "Could not open file " + filename << std::endl;
-    return 1;
-  }
+  if (filename == "-") {
+    std::string strRead{};
+    while (std::getline(std::cin, strRead)) {
+      std::cout << strRead << '\n';
+    }
+  } else {
+    std::ifstream file{filename};
+    if (!file) {
+      std::cerr << "Could not open file " + filename << std::endl;
+      return 1;
+    }
 
-  std::string strRead{};
-  while (std::getline(file, strRead)) {
-    std::cout << strRead << '\n';
+    std::string strRead{};
+    while (std::getline(file, strRead)) {
+      std::cout << strRead << '\n';
+    }
   }
 
   return 0;
